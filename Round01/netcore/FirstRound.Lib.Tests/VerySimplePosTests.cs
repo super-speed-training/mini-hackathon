85a286c66a585bad31c2a712d8acfb11359617e1
Teerachai Laothong <tlaothong@gmail.com>
2018-11-09 22:30:40 +0700

    Added a initial structure for Round01

---
+++ Round01/netcore/FirstRound.Lib.Tests/VerySimplePosTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace FirstRound.Lib.Tests
+{
+    public class VerySimplePosTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
