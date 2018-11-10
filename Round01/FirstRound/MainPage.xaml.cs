522591c4a456cbfbad00606c8bd5bc502e033bb8
Somkiet <somsor@perfenterprise.com>
2018-11-10 16:29:22 +0700

    ui

--- Round01/FirstRound/MainPage.xaml.cs
+++ Round01/FirstRound/MainPage.xaml.cs
@@ -30,23 +30,58 @@ namespace FirstRound
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            var xx = new VerySimplePOS();
-            var amount = double.Parse(Amount.Text);
-            var pay = double.Parse(Pay.Text);
-            var change = xx.ComputeChange(amount, pay);
-            var result = xx.GetChangeBankNotesAndCoins(change);
-            Change.Text = result.RoundedChange.ToString();
-            int value = 0;
-            if (result.BankNotesAndCoins.TryGetValue(BankNotesAndCoinsInSatang.Thousand, out value)) Thoundsand.Text = value.ToString();
-            if (result.BankNotesAndCoins.TryGetValue(BankNotesAndCoinsInSatang.FiveHundreds, out value)) FiveHundreds.Text = value.ToString();
-            if (result.BankNotesAndCoins.TryGetValue(BankNotesAndCoinsInSatang.Hundred, out value)) Hundred.Text = value.ToString();
-            if (result.BankNotesAndCoins.TryGetValue(BankNotesAndCoinsInSatang.Fifty, out value)) Fifty.Text = value.ToString();
-            if (result.BankNotesAndCoins.TryGetValue(BankNotesAndCoinsInSatang.Twenty, out value)) Twenty.Text = value.ToString();
-            if (result.BankNotesAndCoins.TryGetValue(BankNotesAndCoinsInSatang.Ten, out value)) Ten.Text = value.ToString();
-            if (result.BankNotesAndCoins.TryGetValue(BankNotesAndCoinsInSatang.Five, out value)) Five.Text = value.ToString();
-            if (result.BankNotesAndCoins.TryGetValue(BankNotesAndCoinsInSatang.One, out value)) One.Text = value.ToString();
-            if (result.BankNotesAndCoins.TryGetValue(BankNotesAndCoinsInSatang.Fiftieth, out value)) Fiftieth.Text = value.ToString();
-            if (result.BankNotesAndCoins.TryGetValue(BankNotesAndCoinsInSatang.TwentyFifth, out value)) TwentyFifth.Text = value.ToString();
+            var verySimple = new VerySimplePOS();
+            if (double.TryParse(Amount.Text, out double amount) && double.TryParse(Pay.Text, out double pay))
+            {
+                var change = verySimple.ComputeChange(amount, pay);
+                if (change < 0)
+                {
+                    status.Text = "จำนวนเงินไม่พอจ่าย";
+                }
+                else
+                {
+                    status.Text = "จำนวนเงินที่จ่ายพอ";
+                    var result = verySimple.GetChangeBankNotesAndCoins(change);
+                    Change.Text = result.RoundedChange.ToString();
+                    int value = 0;
+                    if (result.BankNotesAndCoins.TryGetValue(BankNotesAndCoinsInSatang.Thousand, out value)) Thoundsand.Text = value.ToString();
+                    if (result.BankNotesAndCoins.TryGetValue(BankNotesAndCoinsInSatang.FiveHundreds, out value)) FiveHundreds.Text = value.ToString();
+                    if (result.BankNotesAndCoins.TryGetValue(BankNotesAndCoinsInSatang.Hundred, out value)) Hundred.Text = value.ToString();
+                    if (result.BankNotesAndCoins.TryGetValue(BankNotesAndCoinsInSatang.Fifty, out value)) Fifty.Text = value.ToString();
+                    if (result.BankNotesAndCoins.TryGetValue(BankNotesAndCoinsInSatang.Twenty, out value)) Twenty.Text = value.ToString();
+                    if (result.BankNotesAndCoins.TryGetValue(BankNotesAndCoinsInSatang.Ten, out value)) Ten.Text = value.ToString();
+                    if (result.BankNotesAndCoins.TryGetValue(BankNotesAndCoinsInSatang.Five, out value)) Five.Text = value.ToString();
+                    if (result.BankNotesAndCoins.TryGetValue(BankNotesAndCoinsInSatang.One, out value)) One.Text = value.ToString();
+                    if (result.BankNotesAndCoins.TryGetValue(BankNotesAndCoinsInSatang.Fiftieth, out value)) Fiftieth.Text = value.ToString();
+                    if (result.BankNotesAndCoins.TryGetValue(BankNotesAndCoinsInSatang.TwentyFifth, out value)) TwentyFifth.Text = value.ToString();
+                }
+            }
+            else
+            {
+                status.Text = "กรุณากรอกจำนวนเงินให้ถูกต้อง";
+            }
+        }
+
+        private void Button_Reset_Click(object sender, RoutedEventArgs e)
+        {
+            Amount.Text = Pay.Text = status.Text = Change.Text = Thoundsand.Text =
+                FiveHundreds.Text = Hundred.Text = Fifty.Text = Twenty.Text = Ten.Text =
+                Five.Text = One.Text = Fiftieth.Text = TwentyFifth.Text = "";
+        }
+
+        private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBlock_SelectionChanged_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBlock_SelectionChanged_2(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
