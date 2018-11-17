6c46cd157f023744af0b9372ede194b4929b009e
Earn <earndarkness@perfenterprise.com>
2018-11-17 15:48:50 +0700

    add celcius project display on ui

--- Round02/SecondRound/MainPage.xaml.cs
+++ Round02/SecondRound/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using Emmellsoft.IoT.Rpi.SenseHat;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
@@ -12,6 +13,10 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.Threading.Tasks;
+using Windows.UI;
+using System.Diagnostics;
+using Microsoft.Azure.Devices.Client;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +27,34 @@ namespace SecondRound
     /// </summary>
     public sealed partial class MainPage : Page
     {
+ 
         public MainPage()
         {
             this.InitializeComponent();
+
+            Task.Run(async () =>
+            {
+                var hat = await SenseHatFactory.GetSenseHat().ConfigureAwait(true);
+                hat.Display.Fill(Colors.Aqua);
+                hat.Display.Update();
+                hat.Sensors.HumiditySensor.Update();
+               
+
+                while (true)
+                {
+                    hat.Sensors.HumiditySensor.Update();
+                    var currentTemp = hat.Sensors.Temperature.GetValueOrDefault();
+                    Debug.WriteLine(currentTemp);
+                    await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                    {
+                        DisplayTemp.Text = currentTemp.ToString("#.##");
+                    });
+                    await Task.Delay(1000);
+                }
+            }).ConfigureAwait(false);
+
+
+
         }
     }
 }
